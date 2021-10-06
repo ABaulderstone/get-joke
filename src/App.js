81f5c085017ab949272669b7daa0815eadb6c894
Alex baulderstone <alex.baulderstone@coderacademy.edu.au>
2021-10-06 17:00:48 +1100

    added loading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const App = () => {
-  return (
-    <div >
-          The basic empty template
-    </div>
-  )
+class App extends Component {
+  state = {
+    joke: "",
+    error: null,
+    loading: false
+
+  }
+
+  getJoke = () => {
+    this.setState({loading: true})
+    fetch("https://icanhazdadjoke.com", {
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => this.setState({joke: data.joke}))
+    .catch(error => this.setState({error: error.message}))
+    .finally(() => this.setState({loading: false}))
+  }
+
+  componentDidMount() {
+    this.getJoke();
+  }
+  
+  
+
+  render() {
+    const {loading, error, joke} = this.state;
+    return(
+      <>
+      {error && <p>{error}</p>}
+      {loading ? <h3>Loading</h3> : <p>{joke}</p>}
+      <button onClick={this.getJoke}>Get a joke</button>
+      </>
+    )
+  }
 }
+
 
 export default App
